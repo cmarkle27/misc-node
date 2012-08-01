0e4321403ab20c985ea8a45477d6456ba63af835
Chris Markle <markle976@cdmarkle-2.local>
2012-08-01 13:47:35 -0400

    http server example

--- server/app.js
+++ server/app.js
@@ -1,19 +1,20 @@
 var http = require("http"), // utilities for working with file paths
 	path = require("path"), // utilities for accessing the file system
-	fs = require("fs");
+	fs = require("fs"),
+	extensions = { ".html": "text/html", ".css": "text/css", ".js": "application/javascript", ".png": "image/png", ".gif": "image/gif", ".jpg": "image/jpeg" };
 
 http.createServer(function(req, res) {
-	// look for a filename in the URL, default to index.html
+
 	var filename = path.basename(req.url) || "index.html",
 		ext = path.extname(filename),
-		localPath = __dirname + "/www/"; // __dirname is a built-in variable containing the path where the code is running
+		dir = path.dirname(req.url).substring(1),
+		localPath = __dirname + "/www/";
 
-	if (ext == ".html") {
-		localPath += filename;
-		// verify that this file actually exists and load it, or else return a 404
+	if (extensions[ext]) {
+		localPath += (dir ? dir + "/" : "") + filename;
 		path.exists(localPath, function(exists) {
 			if (exists) {
-				getFile(localPath, res);
+				getFile(localPath, extensions[ext], res);
 			} else {
 				res.writeHead(404);
 				res.end();
@@ -23,11 +24,14 @@ http.createServer(function(req, res) {
 
 }).listen(8000);
 
-function getFile(localPath, res) {
+function getFile(localPath, mimeType, res) {
 	// read the file in and return it, or return a 500 if it can't be read
 	fs.readFile(localPath, function(err, contents) {
 		if (!err) {
-			// use defaults instead of res.writeHead()
+			res.writeHead(200, {
+				"Content-Type": mimeType,
+				"Content-Length": contents.length
+			});
 			res.end(contents);
 		} else {
 			res.writeHead(500);
