5a7074e0339facfc7b5b81fedd4f156700be7fb2
Chris Markle <markle976@cdmarkle-2.local>
2012-08-03 13:04:12 -0400

    Step 3.6

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
-var connect = require("connect");
+var http = require("http"),
+  querystring = require("querystring");
 
-connect(
-  connect['static'](__dirname + "/www"),
-  connect.bodyParser(),
-  function(req, res) {
-    var userName = req.body.firstName + " " + req.body.lastName,
-      html = "<!doctype html>" +
-        "<html><head><title>Hello " + userName + "</title></head>" +
-        "<body><h1>Hello, " + userName + "!</h1></body></html>";
+http.createServer(function(req, res) {
+  var qs = querystring.parse(req.url.split("?")[1]),
+    username = qs.firstName + " " + qs.lastName,
+    json;
 
-    res.end(html);
+  if (qs.callback) {
+    json = qs.callback + "({username:'" + username + "'});";
+  } else {
+    json = JSON.stringify({"username":username});
   }
-).listen(8000);
+
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "Content-Length": json.length
+  });
+  res.end(json);
+}).listen(8000);
