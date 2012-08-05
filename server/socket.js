ebd3ee0aee22124c6168cd434d7d2ced9db03903
Chris Markle <markle976@cdmarkle-2.(none)>
2012-08-05 12:14:27 -0400

    Revert "Revert "Stop. Socket time.""
    This reverts commit 8776f580d7094428d32bb937f8879cd70a8370c8.

---
+++ server/socket.js
@@ -0,0 +1,12 @@
+var connect = require("connect"),
+  io = require("socket.io").listen(1337);
+
+connect(connect['static'](__dirname + "/www")).listen(8000);
+
+io.sockets.on("connection", function(socket) {
+  socket.on("setName", function(data) {
+    var userName = data.firstName + " " + data.lastName;
+    socket.emit("nameSet", {userName: userName});
+    console.log(userName);
+  });
+});
