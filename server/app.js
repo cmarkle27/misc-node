a54494a612ed388a7e95c1be569d32e1b17a80b8
Chris Markle <markle976@cdmarkle-2.(none)>
2012-08-06 19:39:25 -0400

    cleanup

--- server/app.js
+++ server/app.js
@@ -1,20 +1,3 @@
-var http = require("http"),
-	querystring = require("querystring");
+var connect = require("connect");
 
-http.createServer(function(req, res) {
-	var qs = querystring.parse(req.url.split("?")[1]),
-		username = qs.firstName + " " + qs.lastName,
-		json;
-
-	if (qs.callback) {
-		json = qs.callback + "({username:'" + username + "'});";
-	} else {
-		json = JSON.stringify({"username":username});
-	}
-
-	res.writeHead(200, {
-		"Content-Type": "application/json",
-		"Content-Length": json.length
-	});
-	res.end(json);
-}).listen(8000);
+connect(connect['static'](__dirname + "/www")).listen(8000);
